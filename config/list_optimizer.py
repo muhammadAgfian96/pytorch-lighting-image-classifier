cb1ddb2028ab718c2e04d7df94887f0ff72e7ac4
muhammadagfian96 <muhammadagfian96@gmail.com>
2022-09-06 19:56:22 +0800

    fix config features

---
+++ config/list_optimizer.py
@@ -0,0 +1,14 @@
+
+class ListOptimizer(object):
+    SGD = 'SGD'
+    Adam = 'Adam'
+    AdamW = 'AdamW'
+    Nadam = 'Nadam'
+    Radam = 'Radam'
+    AdamP = 'AdamP'
+    SGDP = 'SGDP'
+    Adadelta = 'Adadelta'
+    Adafactor = 'Adafactor'
+    ADAHESSIAN = 'ADAHESSIAN'
+    RMSprop = 'RMSprop'
+    NovoGrad = 'NovoGrad'
