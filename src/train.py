65b14bbde8019b529a8a06829570d822fdbf6834
muhammadagfian96 <muhammadagfian96@gmail.com>
2022-09-11 22:02:46 +0800

    update

--- src/train.py
+++ src/train.py
@@ -11,15 +11,6 @@ from config.list_models import list_models
 from config.list_optimizer import ListOptimizer
 from src.helper.utils import override_config
 
-# copy to fix file
-import clearml
-path_clearml_package = os.path.join(clearml.__path__[0], 'storage', 'helper.py')
-from_helper_fix = os.path.join(os.getcwd(), 'clearml-fix-hardcore', 'helper.py')
-if os.path.exists(path_clearml_package):
-    print(f'COPY FILE{from_helper_fix} to {path_clearml_package}')
-    shutil.copy2(src=from_helper_fix, dst=path_clearml_package)
-
-import clearml
 from clearml import Task
 
 conf = TrainingConfig()
