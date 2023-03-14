5204c8da6aae8a15b4429302d3ec563def9367b6
muhammadagfian96 <muhammadagfian96@gmail.com>
2023-03-15 00:17:57 +0800

    reqs fix

--- src/train.py
+++ src/train.py
@@ -23,7 +23,7 @@ from pytorch_lightning.tuner import tuning
 # ClearML Setup
 # ----------------------------------------------------------------------------------
 # Task.add_requirements(f"-r {os.path.join(cwd,'docker/requirements.txt')}")
-Task.force_requirements_env_freeze(False, '/workspace/docker/requirements.txt')
+Task.force_requirements_env_freeze(False, '/workspace/requirements.txt')
 
 task = Task.init(
     project_name='Image Classifier/Template',
@@ -31,7 +31,6 @@ task = Task.init(
     task_type=Task.TaskTypes.training,
     auto_connect_frameworks=False
 )
-Task.add_requirements('docker/requirements.txt')
 Task.current_task().set_script(
     repository='https://github.com/muhammadAgfian96/pytorch-lighting-image-classifier.git',
     branch='main',
